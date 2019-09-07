2256b7126c8c0dd5497e43455d5a4092845a3789
emilykocurek <emilykocurek17@gmail.com>
2019-09-07 03:58:59 +0000

    Blur3 complete

--- Image_Blurs/blur3.rb
+++ Image_Blurs/blur3.rb
@@ -10,8 +10,8 @@ class Image
     end
   end
 
-  def blur(stretch=n)
-    stretch.times do
+  def blur(distance)
+    distance.times do
       blur_pixel!
     end
   end
@@ -19,13 +19,14 @@ class Image
   private
 
     def blur_pixel!
+      
       blur_pixel = []
       @picture.each_with_index do |row, row_index|
         row.each_with_index do |column, column_index|
           blur_pixel << [row_index, column_index] if column == 1
         end
       end
-
+      
       blur_pixel.each do |pixel|
         @picture[pixel[0]][pixel[1] + 1] = 1 if pixel[1] + 1 <= @picture[pixel[0]].length - 1
         @picture[pixel[0]][pixel[1] - 1] = 1 if pixel[1] - 1 >= 0
@@ -37,12 +38,18 @@ class Image
 end
 
 image = Image.new([
-  [0, 0, 0, 0],
-  [0, 0, 1, 0],
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1],
 ])
-image.blur!(1)
+image.blur(3)
 image.output_image
